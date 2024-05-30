dc6fda6d56b69a582cbfedcc19400c6f1c9e13d9
proju <projucti2k9@gmail.com>
2024-05-30 19:57:47 +0200

    added MySQL database connection

--- src/main/java/com/projucti/dspring/controller/personcontroller.java
+++ src/main/java/com/projucti/dspring/controller/personcontroller.java
@@ -1,13 +1,11 @@
 package com.projucti.dspring.controller;
 
-import com.projucti.dspring.service.personservice;
 import com.projucti.dspring.model.person;
-
+import com.projucti.dspring.service.personservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -21,26 +19,26 @@ public class personcontroller {
 
     @PostMapping("/persons")
     public String createperson(){
-        person p1= new person();
-        p1.setFirstName("Parizaad");
-        p1.setLastName("Musa");
-        p1.setId(2024001);
-        p1.setAge(1);
-        personService.createperson(p1);
-
-        person p2= new person();
-        p2.setFirstName("Abu Saleh Md");
-        p2.setLastName("Musa");
-        p2.setAge(32);
-        p2.setId(2024002);
-        personService.createperson(p2);
-
-        person p3= new person();
-        p3.setFirstName("Taslima");
-        p3.setLastName("Akter");
-        p3.setAge(32);
-        p3.setId(2024003);
-        personService.createperson(p3);
+//        person p1= new person();
+//        p1.setFirstName("Parizaad");
+//        p1.setLastName("Musa");
+//        p1.setId(2024001L);
+//        p1.setAge(1);
+//        personService.createperson(p1);
+//
+//        person p2= new person();
+//        p2.setFirstName("Abu Saleh Md");
+//        p2.setLastName("Musa");
+//        p2.setAge(32);
+//        p2.setId(2024002L);
+//        personService.createperson(p2);
+//
+//        person p3= new person();
+//        p3.setFirstName("Taslima");
+//        p3.setLastName("Akter");
+//        p3.setAge(32);
+//        p3.setId(2024003);
+//        personService.createperson(p3);
 
         return "person name added";
     }
--- src/main/java/com/projucti/dspring/model/person.java
+++ src/main/java/com/projucti/dspring/model/person.java
@@ -1,10 +1,29 @@
 package com.projucti.dspring.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class person {
     private String firstName;
     private String lastName;
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
+
+    public person() {
+    }
+
+    public person(String firstName, String lastName, Long id, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.age = age;
+    }
+
 
     public int getAge() {
         return age;
@@ -14,11 +33,11 @@ public class person {
         this.age = age;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
