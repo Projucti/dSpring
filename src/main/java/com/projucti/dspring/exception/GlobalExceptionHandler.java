d19502c8838e22c24de96a396ec555dcb076cced
proju <projucti2k9@gmail.com>
2024-06-18 07:35:40 +0200

    adding validation exception handler class

---
+++ src/main/java/com/projucti/dspring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.projucti.dspring.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidExceptions(MethodArgumentNotValidException exception){
+       // Map<String, String> exceptionMap= new HashMap<>();
+
+        return new ResponseEntity<>(exceptionMap, HttpStatus.BAD_REQUEST);
+    }
+}
