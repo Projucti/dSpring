c289abe9f727fde77743b1949453b26d45abb7fa
proju <projucti2k9@gmail.com>
2024-07-01 14:49:05 +0200

    test case added for a new person insertion

---
+++ src/test/java/com/projucti/dspring/PersonServiceImpTest.java
@@ -0,0 +1,48 @@
+package com.projucti.dspring;
+
+import com.projucti.dspring.model.Person;
+import com.projucti.dspring.repository.PersonRepository;
+import com.projucti.dspring.service.PersonServiceImp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class PersonServiceImpTest {
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonServiceImp personServiceImp;
+
+    @BeforeEach
+    public void SetUp()
+    {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testAddPerson(){
+        Person person=new Person();
+        person.setFirstName("Jane");
+        person.setLastName("John");
+        person.setAge(30);
+
+        when(personRepository.save(any(Person.class))).thenReturn(person);
+
+        Person addedPerson= personServiceImp.addPerson(person);
+
+        assertNotNull(addedPerson);
+        assertEquals(addedPerson.getFirstName(),"Jane");
+        assertEquals(addedPerson.getLastName(),"John");
+        assertEquals(addedPerson.getAge(),30);
+
+        verify(personRepository, times(1)).save(person);
+    }
+}
